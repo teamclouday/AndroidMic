1535c046167c51c5a1cb41300057d14d3976fb92
Sida <sida@kulamind.com>
2025-03-23 20:53:28 -0400

    v2.1.3

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 9
-        versionName = "2.1.2"
+        versionName = "2.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables.useSupportLibrary = true
