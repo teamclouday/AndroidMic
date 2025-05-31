9dafe734107f989eabb3fab07a9f7ad798ae8032
teamclouday <teamclouday@gmail.com>
2025-06-01 01:29:10 +0800

    fix resampling code and enable resampling

--- RustApp/src/audio/resampler.rs
+++ RustApp/src/audio/resampler.rs
@@ -61,8 +61,8 @@ where
     let resampled_buffer = if format.sample_rate.to_number() == packet.sample_rate {
         buffer
     } else {
-        bail!("resampling not supported yet! please make sure the sample rate is the same.");
-        // resample_f32_mono_stream(&buffer, packet.sample_rate, format.sample_rate.to_number())?
+        // bail!("resampling not supported yet! please make sure the sample rate is the same.");
+        resample_f32_stream(&buffer, packet.sample_rate, format.sample_rate.to_number())?
     };
 
     // finally convert to output format
@@ -192,48 +192,70 @@ where
 struct ResamplerCache {
     input_rate: u32,
     output_rate: u32,
+    num_channels: usize,
+    sample_buffer: Vec<Vec<f32>>,
     resampler: rubato::FastFixedIn<f32>,
 }
 
 static RESAMPLER_CACHE: Lazy<Mutex<Option<ResamplerCache>>> = Lazy::new(|| Mutex::new(None));
 
-fn resample_f32_mono_stream(
-    data: &[f32],
+fn resample_f32_stream(
+    data: &[Vec<f32>],
     input_sample_rate: u32,
     output_sample_rate: u32,
-) -> anyhow::Result<Vec<f32>> {
+) -> anyhow::Result<Vec<Vec<f32>>> {
+    let chunk_size = 1024;
     let resample_ratio = output_sample_rate as f64 / input_sample_rate as f64;
     let mut resampler_cache = RESAMPLER_CACHE.lock().unwrap();
 
     if resampler_cache.is_none()
         || resampler_cache.as_ref().unwrap().input_rate != input_sample_rate
         || resampler_cache.as_ref().unwrap().output_rate != output_sample_rate
+        || resampler_cache.as_ref().unwrap().num_channels != data.len()
     {
         let resampler = rubato::FastFixedIn::<f32>::new(
             resample_ratio,
             1.0,
             rubato::PolynomialDegree::Cubic,
-            data.len().min(512),
-            1,
+            chunk_size,
+            data.len(),
         )?;
 
         *resampler_cache = Some(ResamplerCache {
             input_rate: input_sample_rate,
             output_rate: output_sample_rate,
+            num_channels: data.len(),
+            sample_buffer: (0..data.len())
+                .map(|_| Vec::with_capacity(chunk_size))
+                .collect(),
             resampler,
         });
     }
 
-    let delay = resampler_cache.as_ref().unwrap().resampler.output_delay();
+    let cache = resampler_cache.as_mut().unwrap();
+    let mut output: Vec<Vec<f32>> = vec![Vec::new(); data.len()];
 
-    let input = vec![data];
-    let output = resampler_cache
-        .as_mut()
-        .unwrap()
-        .resampler
-        .process(&input, None)?
-        .pop()
-        .unwrap();
+    // Create append new data into the cache
+    for channel_idx in 0..data.len() {
+        cache.sample_buffer[channel_idx].extend_from_slice(&data[channel_idx]);
+    }
 
-    Ok(output[delay..].to_vec())
+    while cache.sample_buffer[0].len() >= chunk_size {
+        let chunk_output = cache.resampler.process(&cache.sample_buffer, None)?;
+
+        // Clear the sample buffer for the next round
+        for channel in &mut cache.sample_buffer {
+            channel.drain(0..chunk_size);
+        }
+
+        // Append the resampled data to the output
+        for (channel_idx, channel_data) in chunk_output.iter().enumerate() {
+            if channel_idx < output.len() {
+                output[channel_idx].extend_from_slice(channel_data);
+            }
+        }
+    }
+
+    // For each channel, skip the delay samples
+    Ok(output)
 }
