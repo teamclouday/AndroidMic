c7c5269dd27c002837f90dc78b09618194dbd2f7
teamclouday <teamclouday@gmail.com>
2025-06-09 22:49:56 +0800

    v2.1.6

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 23
         targetSdk = 35
         versionCode = 9
-        versionName = "2.1.5"
+        versionName = "2.1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables.useSupportLibrary = true
