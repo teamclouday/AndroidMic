9889da87634a1c711d47c309d6fd9b4bbbcde53e
teamclouday <teamclouday@gmail.com>
2026-06-08 21:20:43 +0800

    bump version to 2.2.8

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.github.teamclouday.AndroidMic"
         minSdk = 23
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.2.7"
+        versionCode = 24
+        versionName = "2.2.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables.useSupportLibrary = true
